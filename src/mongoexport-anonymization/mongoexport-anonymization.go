ce48b1292c2a307164a80ec6e83849cef14e56c9
Guillaume Arnaud <garnaud@voyages-sncf.com>
2017-06-15 09:55:53 +0200

    a lot of changes

--- src/mongoexport-anonymization/mongoexport-anonymization.go
+++ src/mongoexport-anonymization/mongoexport-anonymization.go
@@ -2,23 +2,30 @@ package main
 
 import (
 	"bufio"
+	"compress/gzip"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"time"
 
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/gorilla/handlers"
+	_ "github.com/mkevac/debugcharts"
 )
 
 type counter struct {
-	iterations       int32
-	unmarshalledDocs int32
-	writedDocs       int32
+	iterations int32
 }
 
 type channels struct {
@@ -30,6 +37,11 @@ type channels struct {
 	lastIds            chan string // channel for last ids
 }
 
+// Closeabe type for easily close all resources
+type Closeable interface {
+	Close() (err error)
+}
+
 var (
 	uri           = flag.String("uri", "mongodb://localhost:27017", "mongo uri")
 	dbName        = flag.String("database", "test", "database name")
@@ -38,26 +50,31 @@ var (
 	unmarshallers = flag.Int("unmarshallers", 20, "number of unmarshallers")
 	writers       = flag.Int("writers", 40, "number of writers")
 	limit         = flag.Int("limit", 100000, "number max of documents")
-	pages         = flag.Int("pages", 10, "number of parallel mongo queries. For each page query, limit will be 'limit/pages'.")
+	pages         = flag.Int("pages", -1, "number of parallel mongo queries. For each page query, limit will be 'limit/pages'. If undefined, pages=GOMAXPROC")
 	batch         = flag.Int("batch", 100, "batch size for mongo requests")
 	prefetch      = flag.Float64("prefetch", 0.5, "prefetch ratio from batch for mongo requests")
-	output        = flag.String("output", "./results/", "path to output directory")
+	output        = flag.String("output", "./results", "path to output directory")
 	prefix        = flag.String("prefix", "export-", "prefix of result files")
+	compressed    = flag.Bool("compressed", false, "compressed")
+	monitored     = flag.Bool("monitored", false, "display current status each second")
+	monitorPort   = flag.String("monitor-port", "50080", "monitor port for go runtime metrics (ex: localhost:50080/metrics)")
 	help          = flag.Bool("help", false, "help")
-	counters      = &counter{iterations: 0, unmarshalledDocs: 0, writedDocs: 0}
+	counters      = &counter{iterations: 0}
 	stopped       = false
 	allChannels   = &channels{}
 )
 
+// replaceIfPresent replace field of the document if exists by given value
 func replaceIfPresent(element bson.M, field string, value string) {
 	if _, ok := element[field]; ok {
 		element[field] = value
 	}
 }
 
-func filter(unfilteredDoc bson.M) {
+// anonymize some fields of the document
+func anonymize(document bson.M) {
 
-	if customers, ok := unfilteredDoc["customers"]; ok {
+	if customers, ok := document["customers"]; ok {
 		for _, customer := range customers.([]interface{}) {
 			replaceIfPresent(customer.(bson.M), "iuc", "xxx")
 			replaceIfPresent(customer.(bson.M), "firstname", "prenom")
@@ -65,7 +82,7 @@ func filter(unfilteredDoc bson.M) {
 		}
 	}
 
-	for _, serviceItem := range unfilteredDoc["serviceItems"].([]interface{}) {
+	for _, serviceItem := range document["serviceItems"].([]interface{}) {
 
 		if contact, ok := serviceItem.(bson.M)["contactInformation"]; ok {
 			fields := []string{"firstname", "name", "address1", "address2", "address3", "address4", "city", "zipCode", "country", "mobilePhoneNumber", "emailAddress"}
@@ -108,24 +125,12 @@ func filter(unfilteredDoc bson.M) {
 	}
 }
 
-func unmarshall(index int) {
-	fmt.Printf("start unmarshalling step: %v\n", index)
-	for raw := range allChannels.outRaws {
-		var unmarshalledDoc bson.M
-		raw.Unmarshal(&unmarshalledDoc)
-		counters.unmarshalledDocs++
-		allChannels.inUnmarshalledDocs <- unmarshalledDoc
-	}
-	fmt.Printf("end of unmarshalling docs: %v\n", index)
-	panic("shouldn't stop to unmarshall documents")
-}
-
+// monitor current export status
 func monitor() {
-	oldCounter := &counter{writedDocs: counters.writedDocs, iterations: counters.iterations, unmarshalledDocs: counters.unmarshalledDocs}
+	oldCounter := &counter{iterations: counters.iterations}
 	for {
-		currentCounter := &counter{writedDocs: counters.writedDocs, iterations: counters.iterations, unmarshalledDocs: counters.unmarshalledDocs}
-		fmt.Printf("read docs: %d, unmarshalled docs: %d, writed docs: %d, ", currentCounter.iterations-oldCounter.iterations, currentCounter.unmarshalledDocs-oldCounter.unmarshalledDocs, currentCounter.writedDocs-oldCounter.writedDocs)
-		fmt.Printf("waiting to unmarshall docs: %d, waiting to write docs: %d\n", len(allChannels.outRaws), len(allChannels.inUnmarshalledDocs))
+		currentCounter := &counter{iterations: counters.iterations}
+		fmt.Printf("docs: %d\n", currentCounter.iterations-oldCounter.iterations)
 		if stopped {
 			break
 		}
@@ -134,22 +139,38 @@ func monitor() {
 	}
 }
 
+// writer returns a gzip or bufio writer depending of user parameter
+func writer(page int) (w io.Writer, closeables []Closeable) {
+	if *compressed {
+		fileName := *output + string(filepath.Separator) + *prefix + strconv.Itoa(page) + ".gz"
+		file, _ := os.Create(fileName)
+		gz := gzip.NewWriter(file)
+		gz.Name = *prefix + strconv.Itoa(page)
+		w = gz
+		closeables = []Closeable{gz, file}
+		fmt.Printf("[%d] will write to file %s\n", page, fileName)
+	} else {
+		fileName := *output + string(filepath.Separator) + *prefix + strconv.Itoa(page) + ".json"
+		file, _ := os.Create(fileName)
+		w = bufio.NewWriter(file)
+		closeables = []Closeable{file}
+		fmt.Printf("[%d] will write to file %s\n", page, fileName)
+	}
+	return w, closeables
+}
+
+// iterate on given page. A page is part a of the whole collection.
 func iterate(page int) {
 	pageLimit := *limit / *pages
 	lastId := <-allChannels.lastIds
-	fmt.Printf("start iteration from mongo _id: %s\n", lastId)
+	fmt.Printf("[%d] start iteration from mongo _id: %s\n", page, lastId)
 	db := createDB()
 	iter := db.C(*coll).Find(bson.M{"_id": bson.M{"$gte": lastId}}).Prefetch(*prefetch).Batch(*batch).Sort("_id").Limit(pageLimit).Iter()
 
 	// create output file for this index
-	fileName := *output + string(filepath.Separator) + *prefix + strconv.Itoa(page) + ".json"
-	fmt.Printf("will write to file %s\n", fileName)
 	os.Mkdir(*output, os.ModeDir)
-	out, _ := os.Create(fileName)
-	writer := bufio.NewWriter(out)
-	enc := json.NewEncoder(writer)
-	defer out.Sync()
-	defer out.Close()
+	zipWriter, closeables := writer(page)
+	enc := json.NewEncoder(zipWriter)
 
 	var currentRaw bson.Raw
 	var unmarshalledDoc bson.M
@@ -159,16 +180,19 @@ func iterate(page int) {
 		counters.iterations++
 		// unmarshal
 		currentRaw.Unmarshal(&unmarshalledDoc)
-		filter(unmarshalledDoc)
+		anonymize(unmarshalledDoc)
 		enc.Encode(&unmarshalledDoc)
 	}
-	writer.Flush()
+	for _, closeable := range closeables {
+		closeable.Close()
+	}
 
 	allChannels.pages <- true
-	fmt.Printf("%d iteration(s) from id %s \n", itCnt, lastId)
+	fmt.Printf("[%d] %d iteration(s) from id %s \n", page, itCnt, lastId)
 
 }
 
+// createDB mongo database
 func createDB() *mgo.Database {
 	session, err := mgo.Dial(*uri)
 	if err != nil {
@@ -190,6 +214,11 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Window of the oplog (time of last oplog - time of first oplog).\nUsage:\n$ mongooplog-window [options]\nGiven arguments: %v\n", os.Args[0])
 		flag.PrintDefaults()
 		return
+	}
+
+	if *pages == -1 {
+		*pages = runtime.NumCPU()
+		fmt.Printf("%d detected cpu, so will run with %d pages\n", *pages, *pages)
 	}
 
 	log.Println("starting...")
@@ -217,7 +246,9 @@ func main() {
 	allChannels.pages = make(chan bool)
 	allChannels.lastIds = make(chan string, *pages)
 
-	go monitor()
+	if *monitored {
+		go monitor()
+	}
 
 	for page := 0; page < *pages; page++ {
 		go iterate(page)
@@ -243,6 +274,7 @@ func main() {
 	durationId := time.Now().Sub(startTimeId)
 	fmt.Printf("duration for reading all ids: %s\n", durationId.String())
 
+	go http.ListenAndServe(":8080", handlers.CompressHandler(http.DefaultServeMux))
 	fmt.Println("start iteration from mongo")
 	for i := 0; i < *pages; i++ {
 		<-allChannels.pages
